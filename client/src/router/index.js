bc44af990d773822df9a07270a33f3cd1273f5df
MuhammadWildanf <wildanf.daftar@gmail.com>
2023-10-03 23:46:20 +0700

    layouting

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,23 +1,55 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import LandingView from "../views/landingView.vue";
+
+import LoginView from "../views/admin/login.vue";
+import AdminView from "../views/adminView.vue";
+import UserView from "../views/admin/user.vue";
+import kelolaTEmpatWisata from "../views/admin/user.vue";
+import tempatWisata from "../views/admin/user.vue";
+import manajemenWilayah from "../views/admin/user.vue";
+import rekomendasiWIsata from "../views/admin/user.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "landing",
+      component: LandingView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/login",
+      name: "login",
+      component: LoginView,
+    },
+    {
+      path: "/admin",
+      name: "admin",
+      component: AdminView,
+      children: [
+        {
+          path: "/user",
+          component: UserView,
+        },
+        {
+          path: "/kelola-tempat-wisata",
+          component: kelolaTEmpatWisata,
+        },
+        {
+          path: "/tempat-wisata-&-usaha",
+          component: tempatWisata,
+        },
+        {
+          path: "/manajemen-wilayah",
+          component: manajemenWilayah,
+        },
+        {
+          path: "/rekomendasi-wisata",
+          component: rekomendasiWIsata,
+        },
+      ],
+    },
+  ],
+});
 
-export default router
+export default router;
